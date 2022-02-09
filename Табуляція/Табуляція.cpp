139281f636ceea9d151bb772b116950a6ba1fb99
Я <@WIN-0EO50QJFU18>
2022-02-09 16:07:44 +0200

    Добавьте файлы проекта.

---
+++ Табуляція/Табуляція.cpp
@@ -0,0 +1,23 @@
+﻿// Дан рядок символів. Замінити в ньому всі пробіли на табуляцію
+#include <iostream>
+#include <string>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int i = 0;
+    string text;
+    cout << "Вводьте текст :\n";
+    getline(cin, text);
+    while (text[i]!='\0')
+    {
+        if (text[i] == ' ') text[i] = '\t';
+        i++;
+    }
+    cout << text << "\n";
+}
+
+
